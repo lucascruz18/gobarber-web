16b1f01791a34d2da15783fd0c0b501eabf36cd9
Lucas <lucas@alternativainternet.com>
2019-08-19 14:21:35 -0300

    register user

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -14,6 +14,7 @@ export default function auth(state = INITIAL_STATE, action) {
         break;
       }
       case '@auth/SIGN_IN_SUCCESS': {
+        draft.token = action.payload.token;
         draft.profile = action.payload.user;
         draft.signed = true;
         draft.loading = false;
